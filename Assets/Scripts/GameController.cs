98b2385db07dfcd22850d05d4b5092119b978185
niknar266 <krm266@gmail.com>
2015-10-10 14:46:27 +0100

    Assets

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	Player player1, player2;
+	float playerDistance;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+
+    float currentSpeed = 0;
+    float maxIdealSpeed = 80.0f;
+    float minIdealSpeed = 60.0f;
+    float topSpeed = 100.0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
